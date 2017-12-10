e21c2b2511e0ffe2b61e576fdcc11b4d0cd35ad9
veraskokova <vera-skokova11@yandex.ru>
2017-12-11 03:49:07 +0600

    It works somehow

--- NetworksLab7/src/ru/nsu/ccfit/skokova/portforwarder/PortForwarder.java
+++ NetworksLab7/src/ru/nsu/ccfit/skokova/portforwarder/PortForwarder.java
@@ -1,18 +1,29 @@
 package ru.nsu.ccfit.skokova.portforwarder;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class PortForwarder {
+    private static final int BUF_SIZE = 4096;
     private int lPort;
     private int rPort;
     private String rHost;
-
+    private InetSocketAddress inetSocketAddress;
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
+
+    private Map<SocketChannel, SocketChannel> connectionMap = new HashMap<>();
+    private Map<SocketChannel, ByteBuffer> bytesToSend = new HashMap<>();
 
     public PortForwarder(int lPort, int rPort, String rHost) {
         this.lPort = lPort;
@@ -31,18 +42,112 @@ public class PortForwarder {
 
     public void start() {
         try {
+            resolveDns();
+
             serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(new InetSocketAddress(lPort));
             serverSocketChannel.configureBlocking(false);
 
             selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Listening on port " + lPort);
 
             while (true) {
-                selector.select();
+                int ready = selector.select();
+
+                if (ready == 0) {
+                    continue;
+                }
+
+                Iterator iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = (SelectionKey) iterator.next();
+                    iterator.remove();
+                    processRequest(key);
+                }
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private void resolveDns() throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getByName(rHost);
+        inetSocketAddress = new InetSocketAddress(inetAddress, rPort);
+    }
+
+    private void processRequest(SelectionKey selectionKey) throws IOException {
+        if (selectionKey.isAcceptable()) {
+            registerClient();
+        } else if (selectionKey.isReadable()) {
+            processInput(selectionKey);
+        } else if (selectionKey.isWritable()) {
+            processOutput(selectionKey);
+        }/* else if (selectionKey.isConnectable()) {
+            processConnect(selectionKey);
+        }*/
+    }
+
+    private void registerClient() throws IOException {
+        try {
+            SocketChannel clientSocketChannel = serverSocketChannel.accept();
+            System.out.println("New connection from local " + clientSocketChannel.getLocalAddress() + " remote " + clientSocketChannel.getRemoteAddress());
+
+            SocketChannel connectionSocketChannel = SocketChannel.open();
+            connectionSocketChannel.connect(inetSocketAddress);
+
+            connectionSocketChannel.configureBlocking(false);
+            clientSocketChannel.configureBlocking(false);
+
+            clientSocketChannel.register(selector, SelectionKey.OP_READ);
+            connectionSocketChannel.register(selector, SelectionKey.OP_READ);
+
+            connectionMap.put(clientSocketChannel, connectionSocketChannel);
+            connectionMap.put(connectionSocketChannel, clientSocketChannel);
+
+            bytesToSend.put(clientSocketChannel, ByteBuffer.allocate(BUF_SIZE));
+            bytesToSend.put(connectionSocketChannel, ByteBuffer.allocate(BUF_SIZE));
+        } catch (NullPointerException e) {
+            //System.out.println("null caught");
+        }
+    }
+
+    private void processInput(SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        System.out.println("New input from local " + socketChannel.getLocalAddress() + " remote " + socketChannel.getRemoteAddress());
+        ByteBuffer buffer = bytesToSend.get(socketChannel);
+        socketChannel.read(buffer);
+        SocketChannel anotherSocketChannel = connectionMap.get(socketChannel);
+        buffer.flip();
+        anotherSocketChannel.write(buffer);
+        if (buffer.hasRemaining()) {
+            socketChannel.register(selector, SelectionKey.OP_WRITE);
+        }
+        buffer.flip();
+        System.out.println("Wrote data from input");
+    }
+
+    private void processOutput(SelectionKey selectionKey) throws IOException {
+        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+        System.out.println("New output from local " + socketChannel.getLocalAddress() + " remote " + socketChannel.getRemoteAddress());
+        SocketChannel anotherSocketChannel = connectionMap.get(socketChannel);
+        ByteBuffer buffer = bytesToSend.get(anotherSocketChannel);
+        buffer.flip();
+        socketChannel.write(buffer);
+        if (!buffer.hasRemaining()) {
+            selectionKey.cancel();
+        }
+        buffer.flip();
+        System.out.println("Wrote data from output");
+    }
+
+    private void processConnect(SelectionKey selectionKey) throws IOException {
+        SocketChannel connectionSocketChannel = (SocketChannel) selectionKey.channel();
+        connectionSocketChannel.configureBlocking(false);
+        System.out.println("New connection response from remote " + connectionSocketChannel.getRemoteAddress());
+        connectionSocketChannel.register(selector, SelectionKey.OP_READ);
+        SocketChannel clientSocketChannel = connectionMap.get(connectionSocketChannel);
+        clientSocketChannel.configureBlocking(false);
+        clientSocketChannel.register(selector, SelectionKey.OP_READ);
     }
 }
